79a56c29d7c6ffb9f118a895f45e2fea6e16e33d
Zac Clemans <zaclem01@gmail.com>
2016-08-05 11:43:01 -0400

    reorganize home page

--- client/components/Home.js
+++ client/components/Home.js
@@ -23,71 +23,22 @@ class Home extends React.Component {
 		return (
 			<Grid>
 				<Row>
-					<Col
-						md={6}
-						xs={12}
-					>
-						<h2 className="home-callout-secondary">
+					<Col xs={8} xsOffset={2}>
+						<h2 className="home-callout">
 							Simple and easy to use
 						</h2>
-						<p>
+						<p className="home-callout">
 							Create a question and choose the available answers. Thats it!
 						</p>
-						<p>
+						<p className="home-callout">
 							Allow as many voting options as you want
 						</p>
-						<p>
+						<p className="home-callout">
 							Save your polls and share them for the whole world to see!
 						</p>
-						<p>
+						<p className="home-callout">
 							Edit previous polls without losing the current results
 						</p>
-					</Col>
-					<Col
-						md={6}
-						xs={12}
-					>
-						<h2 className="home-callout">
-							Start voting today!
-						</h2>
-						<h2 className="home-callout">
-							Choose an option below:
-						</h2>
-						<hr />
-						<Row>
-							<Col xs={12}>
-								<h4 className="home-navigation">
-									Find an interesting poll and vote now; no signup needed!
-								</h4>
-								<Link to="/browse">
-									<h3 className="home-navigation">
-										Browse Polls
-									</h3>
-								</Link>
-							</Col>
-						</Row>
-						<Row>
-							<Col xs={6}>
-								<h4 className="home-navigation">
-									Create your own polls?
-								</h4>
-								<Link to="/signup">
-									<h3 className="home-navigation">
-										Sign up
-									</h3>
-								</Link>
-							</Col>
-							<Col xs={6}>
-								<h4 className="home-navigation">
-									Already a user?
-								</h4>
-								<Link to="/signin">
-									<h3 className="home-navigation">
-										Log in
-									</h3>
-								</Link>
-							</Col>
-						</Row>
 					</Col>
 				</Row>
 				<Row>
